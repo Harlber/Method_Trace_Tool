f2f39e584cbafab414ad92c82d853b4c67bf7b27
yuchuangu85 <yuchuangu85@gmail.com>
2017-06-15 11:59:46 +0800

    添加Mac OS系统兼容，修改一些问题

--- MethodTrace/src/TraceScanner.java
+++ MethodTrace/src/TraceScanner.java
@@ -4,97 +4,97 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Êñá‰ª∂Êâ´Êèè
+ */
 public class TraceScanner {
-	private File file;
-	private String packageName = "";
-	private AnalysisListener listener;
+    private File file;
+    private String packageName = "";
+    private AnalysisListener listener;
 
-	public TraceScanner(File file) {
-		super();
-		this.file = file;
-		System.out.println("file£∫" + file);
-	}
+    public TraceScanner(File file) {
+        super();
+        this.file = file;
+        System.out.println("fileÔºö" + file);
+    }
 
-	public void setFile(File file) {
-		this.file = file;
-	}
+    public void setFile(File file) {
+        this.file = file;
+    }
 
-	public void setPackageName(String packageName) {
-		this.packageName = packageName;
-	}
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
 
-	public void setListener(AnalysisListener listener) {
-		this.listener = listener;
-	}
+    public void setListener(AnalysisListener listener) {
+        this.listener = listener;
+    }
 
-	public HashMap<String, InfoBean> convertFile(String path) {
-		ArrayList<String> index = new ArrayList<String>();
-		HashMap<String, InfoBean> result = new HashMap<String, InfoBean>();
-		String osName = System.getProperty("os.name");
+    public HashMap<String, InfoBean> convertFile(String path) {
+        ArrayList<String> index = new ArrayList<String>();
+        HashMap<String, InfoBean> result = new HashMap<String, InfoBean>();
+        String osName = System.getProperty("os.name");
 
-		System.out.println("packageName£∫" + packageName);
-		index.add("PackageName: " + packageName + "\n");
-		index.add("start dmtracedump\n");
-		String oName = packageName.replaceAll("[.]", "/");
-		if (listener != null) {
-			listener.startAnalysis();
-		}
-		if (!file.exists()) {
-			System.out.println("File does not exist");
-		} else {
+        Log.e(Log.getTag(),"packageNameÔºö" + packageName);
+        index.add("PackageName: " + packageName + "\n");
+        index.add("start dmtracedump\n");
+        String oName = packageName.replaceAll("[.]", "/");
+        if (listener != null) {
+            listener.startAnalysis();
+        }
+        if (!file.exists()) {
+            System.out.println("File does not exist");
+        } else {
+            Runtime runtime = Runtime.getRuntime();
+            BufferedReader br = null;
+            try {
+                Process p;
+                if (osName.contains("Windows") || osName.contains("windows")) {
+                    p = Runtime.getRuntime().exec(Utils.getWinCommand(path));
+                } else {
+                    // mac or linux
+                    p = Runtime.getRuntime().exec(Utils.getMacCommand(path));
+                }
 
-			Runtime runtime = Runtime.getRuntime();
-			BufferedReader br = null;
-			try {
-				Process p;
-				if (osName.contains("Windows") || osName.contains("windows")) {
-					p = Runtime.getRuntime().exec(Utils.getWinCommand(path));
+                br = new BufferedReader(new InputStreamReader(
+                        p.getInputStream()));
+                String lineTxt = null;
+                // ËØªÂèñÊØèË°åÁöÑÊï∞ÊçÆ
+                while ((lineTxt = br.readLine()) != null) {
+                    if (lineTxt.contains(packageName)
+                            || lineTxt.contains(oName)) {
+                        result = Utils.convert2Tab(result, lineTxt.replaceAll("[.]{2,}+", ""));
+                        index.add(lineTxt + "\n");
+                    }
+                }
+            } catch (Exception e) {
+                result = Utils.convert2Tab(result,
+                        "unknown ent 0 exec dmtracedump exception");
+                index.add("ÊâßË°ådmtracedumpÂëΩ‰ª§ÂºÇÂ∏∏\n");
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                index.add("end dmtracedump\n");
+                System.out.println("cmd end");
+                if (listener != null) {
+                    listener.afterAnalysis();
+                }
+            }
+        }
+        return result;
 
-				} else {
-					// mac or linux
-					p = Runtime.getRuntime().exec(Utils.getMacCommand(path));
-				}
+    }
 
-				br = new BufferedReader(new InputStreamReader(
-						p.getInputStream()));
-				String lineTxt = null;
-				while ((lineTxt = br.readLine()) != null) {
-					if (lineTxt.contains(packageName)
-							|| lineTxt.contains(oName)) {
-						//System.out.println(lineTxt);
-						result = Utils.convert2Tab(result, lineTxt.replaceAll("[.]{2,}+", ""));
-						index.add(lineTxt + "\n");
-					}
-				}
-			} catch (Exception e) {
-				result = Utils.convert2Tab(result,
-						"unknown ent 0 exec dmtracedump exception");
-				index.add("÷¥––dmtracedump√¸¡Ó“Ï≥£\n");
-				e.printStackTrace();
-			} finally {
+    public interface AnalysisListener {
+        void startAnalysis();
 
-				if (br != null) {
-					try {
-						br.close();
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-				index.add("end dmtracedump\n");
-				System.out.println("cmd end");
-				if (listener != null) {
-					listener.afterAnalysis();
-				}
-			}
-		}
-		return result;
-
-	}
-
-	public interface AnalysisListener {
-		void startAnalysis();
-
-		void afterAnalysis();
-	}
+        void afterAnalysis();
+    }
 
 }
